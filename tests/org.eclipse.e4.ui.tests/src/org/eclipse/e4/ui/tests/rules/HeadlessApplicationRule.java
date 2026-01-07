aea81ab70923a115bcea75f2ea0af2419c6b5ed5
Satvik Reddy Amireddy <amireddysatvik19@gmail.com>
2026-01-07 00:05:45 -0800

    Refactor HeadlessApplicationRule to HeadlessApplicationExtension

--- tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/rules/HeadlessApplicationRule.java
+++ tests/org.eclipse.e4.ui.tests/src/org/eclipse/e4/ui/tests/rules/HeadlessApplicationRule.java
@@ -12,56 +12,40 @@
  *     Lars Vogel <Lars.Vogel@vogella.com> - initial API and implementation
  ******************************************************************************/
 
-package org.eclipse.e4.ui.tests.rules;
+package org.eclipse.e4.ui.tests.extensions;
 
 import org.eclipse.e4.core.commands.CommandServiceAddon;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.internal.workbench.swt.E4Application;
 import org.eclipse.e4.ui.services.ContextServiceAddon;
-import org.junit.rules.TestRule;
-import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
+import org.junit.jupiter.api.extension.*;
 
+public class HeadlessApplicationExtension
+        implements BeforeEachCallback, AfterEachCallback {
 
-public class HeadlessApplicationRule implements TestRule {
-	private IEclipseContext applicationContext;
+    private IEclipseContext applicationContext;
 
-	/**
-	 * @return the applicationContext
-	 */
-	public IEclipseContext getApplicationContext() {
-		return applicationContext;
-	}
+    public IEclipseContext getApplicationContext() {
+        return applicationContext;
+    }
 
-	@Override
-	public Statement apply(Statement base, Description description) {
-		return new MyStatement(base);
-	}
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        applicationContext = createApplicationContext();
+    }
 
-	public class MyStatement extends Statement {
-		private final Statement base;
+    @Override
+    public void afterEach(ExtensionContext context) {
+        if (applicationContext != null) {
+            applicationContext.dispose();
+        }
+    }
 
-		public MyStatement(Statement base) {
-			this.base = base;
-		}
-
-		@Override
-		public void evaluate() throws Throwable {
-			applicationContext = createApplicationContext();
-			try {
-				base.evaluate();
-			} finally {
-				applicationContext.dispose();
-			}
-		}
-	}
-
-
-	protected IEclipseContext createApplicationContext() {
-		final IEclipseContext appContext = E4Application.createDefaultContext();
-		ContextInjectionFactory.make(CommandServiceAddon.class, appContext);
-		ContextInjectionFactory.make(ContextServiceAddon.class, appContext);
-		return appContext;
-	}
+    protected IEclipseContext createApplicationContext() {
+        final IEclipseContext appContext = E4Application.createDefaultContext();
+        ContextInjectionFactory.make(CommandServiceAddon.class, appContext);
+        ContextInjectionFactory.make(ContextServiceAddon.class, appContext);
+        return appContext;
+    }
 }
